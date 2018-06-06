183f7e2a449817703a69966c07ff303f52d13caf
li-lin <lin.lee@live.cn>
2018-06-06 22:25:45 +0800

    添加ServerConfig实体类

---
+++ WhichDirection.Domain/Entities/ServerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhichDirection.Domain.Entities
+{
+    /// <summary>
+    /// 服务器配置数据，只允许有一行数据。
+    /// </summary>
+    public class ServerConfig
+    {
+        public int ConfigId { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
